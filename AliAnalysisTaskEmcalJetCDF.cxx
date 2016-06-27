c713ec6d4b17328dcc77ea1879f95c04c3d1d097
asevcenc <adrian.sevcenco@cern.ch>
2016-06-27 15:46:21 +0300

    move function to namespace includind AddTask

--- AliAnalysisTaskEmcalJetCDF.cxx
+++ AliAnalysisTaskEmcalJetCDF.cxx
@@ -18,6 +18,7 @@
 #include <TClonesArray.h>
 #include <TH1D.h>
 #include <TH2D.h>
+#include <TString.h>
 
 #include <AliVCluster.h>
 #include <AliVParticle.h>
@@ -29,6 +30,9 @@
 #include "AliJetContainer.h"
 #include "AliParticleContainer.h"
 #include "AliClusterContainer.h"
+#include "AliAnalysisManager.h"
+#include "AliVEventHandler.h"
+#include "AliAnalysisDataContainer.h"
 
 #include "AliEmcalList.h"
 
@@ -1412,6 +1416,103 @@ std::vector<Int_t> NS_AliAnalysisTaskEmcalJetCDF::SortTracksPt ( AliParticleCont
   for (auto it : pair_list) { index_sorted_list.push_back(it.second); }
 
   return index_sorted_list;
+  }
+
+
+/// Add a AliAnalysisTaskEmcalJetCDF task - detailed signature
+/// \param const char* ntracks : name of tracks collection
+/// \param const char* nclusters : name of clusters collection
+/// \param const char* ncells : name of EMCAL cell collection
+/// \param const char* tag
+/// \return AliAnalysisTaskEmcalJetCDF* task
+TObject* NS_AliAnalysisTaskEmcalJetCDF::AddTaskEmcalJetCDF ( const char* ntracks, const char* nclusters, const char* ncells, const char* tag)
+  {
+  // Get the pointer to the existing analysis manager via the static access method.
+  //==============================================================================
+  AliAnalysisManager* mgr = AliAnalysisManager::GetAnalysisManager();
+  if ( !mgr ) { ::Error ( "AddTaskEmcalJetCDF", "No analysis manager to connect to." );  return NULL; }
+
+  // Check the analysis type using the event handlers connected to the analysis manager.
+  //==============================================================================
+  AliVEventHandler* handler = mgr->GetInputEventHandler();
+  if (!handler) { ::Error ( "AddTaskEmcalJetCDF", "This task requires an input event handler" ); return NULL; }
+
+  enum EDataType_t { kUnknown, kESD, kAOD }; EDataType_t dataType = kUnknown;
+
+  if (handler->InheritsFrom("AliESDInputHandler")) { dataType = kESD; }
+  else
+  if (handler->InheritsFrom("AliAODInputHandler")) { dataType = kAOD; }
+
+  //-------------------------------------------------------
+  // Init the task and do settings
+  //-------------------------------------------------------
+  TString suffix   ( tag );
+  TString tracks   ( ntracks );
+  TString clusters ( nclusters );
+  TString cells    ( ncells );
+
+  if ( tracks.EqualTo("usedefault") )
+    {
+    if ( dataType == kESD ) { tracks = "Tracks"; }
+    else
+    if ( dataType == kAOD ) { tracks = "tracks"; }
+    else
+      { tracks = ""; }
+    }
+
+  if ( clusters.EqualTo("usedefault") )
+    {
+    if ( dataType == kESD ) { clusters = "CaloClusters"; }
+    else
+    if ( dataType == kAOD ) { clusters = "caloClusters"; }
+    else
+      { clusters = ""; }
+    }
+
+  if ( cells.EqualTo("usedefault") )
+    {
+    if (dataType == kESD) { cells = "EMCALCells"; }
+    else
+    if (dataType == kAOD) { cells = "emcalCells"; }
+    else
+      { cells = ""; }
+    }
+
+  TString name("JetCDF");
+  if (!tracks.IsNull())   { name += "_" + tracks; }
+  if (!clusters.IsNull()) { name += "_" + clusters; }
+  if (!cells.IsNull())    { name += "_" + cells; }
+  if (!suffix.IsNull())   { name += "_" + suffix; }
+
+  AliAnalysisTaskEmcalJetCDF* cdfTask = new AliAnalysisTaskEmcalJetCDF ( name.Data() );
+  cdfTask->SetVzRange(-10,10);
+  cdfTask->SetCaloCellsName(cells.Data());
+
+  if ( tracks.EqualTo("mcparticles") )
+    { AliMCParticleContainer* mcpartCont = cdfTask->AddMCParticleContainer ( tracks.Data() ); }
+  else
+  if ( tracks.EqualTo("tracks") || tracks.EqualTo("Tracks") )
+    { AliTrackContainer* trackCont = cdfTask->AddTrackContainer( tracks.Data() ); }
+  else
+  if ( !tracks.IsNull())
+    { cdfTask->AddParticleContainer(tracks.Data()); }
+
+  //-------------------------------------------------------
+  // Final settings, pass to manager and set the containers
+  //-------------------------------------------------------
+  mgr->AddTask ( cdfTask );
+
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput1  = mgr->GetCommonInputContainer();
+
+  TString contname = name + "_histos";
+  TString outfile (Form("%s", AliAnalysisManager::GetCommonFileName()));
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer ( contname.Data(), TList::Class(), AliAnalysisManager::kOutputContainer, outfile.Data() );
+
+  mgr->ConnectInput  ( cdfTask, 0,  cinput1 );
+  mgr->ConnectOutput ( cdfTask, 1, coutput1 );
+
+  return cdfTask;
   }
 
 // kate: indent-mode none; indent-width 2; replace-tabs on;
--- AliAnalysisTaskEmcalJetCDF.h
+++ AliAnalysisTaskEmcalJetCDF.h
@@ -54,52 +54,65 @@ class AliAnalysisTaskEmcalJetCDF : public AliAnalysisTaskEmcalJet
   };
 
 namespace NS_AliAnalysisTaskEmcalJetCDF {
-    /// (pt,index) pair
-    typedef std::pair<Double_t, Int_t> ptidx_pair;
+  /// (pt,index) pair
+  typedef std::pair<Double_t, Int_t> ptidx_pair;
 
-    /// functional for sorting pair by first element - descending
-    struct sort_descend
-      {
-      bool operator () ( const ptidx_pair &p1, const ptidx_pair &p2 )  { return p1.first > p2.first ; }
-      };
+  /// functional for sorting pair by first element - descending
+  struct sort_descend
+    {
+    bool operator () ( const ptidx_pair &p1, const ptidx_pair &p2 )  { return p1.first > p2.first ; }
+    };
 
-    /// Sorting of tracks in the event by pt (descending)
-    /// \param AliVEvent*
-    /// \return vector of indexes of constituents
-    std::vector<Int_t>          SortTracksPt ( AliVEvent *event );
+  /// Sorting of tracks in the event by pt (descending)
+  /// \param AliVEvent*
+  /// \return vector of indexes of constituents
+  std::vector<Int_t>          SortTracksPt ( AliVEvent *event );
 
-    /// Sorting of tracks in the event by pt (descending) - using a particle container
-    /// \param AliParticleContainer*
-    /// \return vector of indexes of constituents
-    std::vector<Int_t>          SortTracksPt ( AliParticleContainer *track_container );
+  /// Sorting of tracks in the event by pt (descending) - using a particle container
+  /// \param AliParticleContainer*
+  /// \return vector of indexes of constituents
+  std::vector<Int_t>          SortTracksPt ( AliParticleContainer *track_container );
 
-    /// Get P() fraction of constituent to jet
-    /// \param AliEmcalJet* jet
-    /// \param AliVParticle* trk
-    /// \return Z = trk->P()/jet->P()
-    Double_t                    Z_ptot ( const AliEmcalJet* jet, const AliVParticle* trk ); // Get Z of constituent trk ; p total
+  /// Get P() fraction of constituent to jet
+  /// \param AliEmcalJet* jet
+  /// \param AliVParticle* trk
+  /// \return Z = trk->P()/jet->P()
+  Double_t                    Z_ptot ( const AliEmcalJet* jet, const AliVParticle* trk ); // Get Z of constituent trk ; p total
 
-    /// Get Pt() fraction of constituent to jet
-    /// \param AliEmcalJet* jet
-    /// \param AliVParticle* trk
-    /// \return Z = trk->Pt()/jet->Pt()
-    Double_t                    Z_pt   ( const AliEmcalJet* jet, const AliVParticle* trk ); // Get Z of constituent trk ; pt
+  /// Get Pt() fraction of constituent to jet
+  /// \param AliEmcalJet* jet
+  /// \param AliVParticle* trk
+  /// \return Z = trk->Pt()/jet->Pt()
+  Double_t                    Z_pt   ( const AliEmcalJet* jet, const AliVParticle* trk ); // Get Z of constituent trk ; pt
 
-    /// Get Xi for a double value z
-    /// \return Xi
-    inline Double_t              Xi ( Double_t z ) { return TMath::Log ( 1/z ); } // Get Xi of value z
+  /// Get Xi for a double value z
+  /// \return Xi
+  inline Double_t              Xi ( Double_t z ) { return TMath::Log ( 1/z ); } // Get Xi of value z
 
-    /// Return dR dinstance in eta,phi plane between 2 AliVParticle derived objects
-    /// \param AliVParticle* particle1
-    /// \param AliVParticle* particle2
-    /// \return distance
-    Double_t                    DeltaR ( const AliVParticle* part1, const AliVParticle* part2 );
+  /// Return dR dinstance in eta,phi plane between 2 AliVParticle derived objects
+  /// \param AliVParticle* particle1
+  /// \param AliVParticle* particle2
+  /// \return distance
+  Double_t                    DeltaR ( const AliVParticle* part1, const AliVParticle* part2 );
 
-    /// Search for index(int) in array of ints
-    /// \param index - the int to be searched
-    /// \param array of ints
-    /// \return kTRUE if found
-    Int_t                      IdxInArray ( Int_t index, TArrayI &array );
+  /// Search for index(int) in array of ints
+  /// \param index - the int to be searched
+  /// \param array of ints
+  /// \return kTRUE if found
+  Int_t                      IdxInArray ( Int_t index, TArrayI &array );
+
+  /// Add a AliAnalysisTaskEmcalJetCDF task - detailed signature
+  /// \param const char* ntracks : name of tracks collection
+  /// \param const char* nclusters : name of clusters collection
+  /// \param const char* ncells : name of EMCAL cell collection
+  /// \param const char* tag
+  /// \return AliAnalysisTaskEmcalJetCDF* task
+  TObject* AddTaskEmcalJetCDF (
+                                const char* ntracks    = "usedefault",
+                                const char* nclusters  = "usedefault",
+                                const char* ncells     = "usedefault",
+                                const char* tag        = "CDF"
+                              );
 
 }
 
